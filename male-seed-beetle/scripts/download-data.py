08f622aa8a2aacb2fb990a088d903de396856267
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2024-12-11 13:17:48 +0100

    feat: update to pathlib and remove input

--- male-seed-beetle/scripts/download-data.py
+++ male-seed-beetle/scripts/download-data.py
@@ -1,48 +1,37 @@
-import os
+from pathlib import Path
 
 import requests
 
-def download_date():
-    """Downloads a data file from a url coded directly in the script.
+#testing:
+url = 'https://datadryad.org/stash/downloads/file_stream/30903'
+
+def download_data(url: str):
+    """Downloads a data file from a given URL.
 
     This is the most basic of the download data scripts. It requires that the url for 
-    the data is written into the script, as well as the folder name 
+    the data is provided as an argument, as well as the folder name 
     for the data resource. It also creates the data-raw folder if that doesn't already
     exist, and creates a .gitignore file in the folder if that doesn't exist.
-    There are two things that needs to be edited when the script is copied to a new
+    There are two things that need to be edited when the script is copied to a new
     folder, the name of the folder in the folder_path variable and the url for the data.
 
     Args:
-        url: the url for the data file given as a hard-coded variable below.
+        url: the url for the data file given as an argument.
 
     Returns:
-        No return as such, but a file is fetched and stored in the repo.
+        No return as such, but a file is fetched and saved to the relevant folder in the repo.
 
     Raises:
         No error messages for now, but will need one if it can't find the file.
     """
 
-script_dir = os.path.dirname(os.path.abspath(__file__)) 
+script_dir = Path(__file__).resolve().parent.parent #testing male-seed-beetle returned with print(f'{script_dir}')
 
-folder_path = os.path.join(script_dir, '..', '..', 'male-seed-beetle', 'data-raw')
+folder_path = script_dir / 'data-raw' #testing male-seed-beetle/data-raw returned with print(f'{folder_path}')
 
-file_path = os.path.join(folder_path, '.gitignore')
+raw_data = requests.get(url, allow_redirects=True)
 
-if not os.path.exists(folder_path):
-    os.mkdir(folder_path)
-else:
-    pass
+file_path = folder_path / 'data.csv'
 
-if not os.path.isfile(file_path):
-    with open(file_path, 'w') as file:
-        file.write('*')
-else:
-    pass
-
-url = 'https://datadryad.org/stash/downloads/file_stream/30903'
-
-r = requests.get(url, allow_redirects=True)
-
-file_path = os.path.join(folder_path, 'data.csv')
 with open(file_path, 'wb') as file:
-    file.write(r.content)
+    file.write(raw_data.content) 
