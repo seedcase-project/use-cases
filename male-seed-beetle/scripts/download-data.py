8ca05a6cf0f5b2231ede8148ab007a77e9ef5905
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2024-12-12 10:30:32 +0100

    feat: remove function from download and add to gitignore file
    add a line to gitignore so that it actually syncs with github

--- male-seed-beetle/scripts/download-data.py
+++ male-seed-beetle/scripts/download-data.py
@@ -2,29 +2,27 @@ from pathlib import Path
 
 import requests
 
-#testing:
-url = 'https://datadryad.org/stash/downloads/file_stream/30903'
+"""Downloads a data file from a given URL.
 
-def download_data(url: str):
-    """Downloads a data file from a given URL.
+This is the most basic of the download data scripts. It requires that the url for 
+the data is provided as an argument, and it assumes that a data-raw folder 
+has been created containing a .gitignore file.
 
-    This is the most basic of the download data scripts. It requires that the url for 
-    the data is provided as an argument, and it assumes that a data-raw folder 
-    has been created containing a .gitignore file.
+Args:
+    url: the url for the data file given as an argument.
 
-    Args:
-        url: the url for the data file given as an argument.
+Returns:
+    No return as such, but a file is fetched and saved to the relevant folder in the repo.
 
-    Returns:
-        No return as such, but a file is fetched and saved to the relevant folder in the repo.
-
-    Raises:
-        No error messages for now, but will need one if it can't find the file.
-    """
+Raises:
+    No error messages for now, but will need one if it can't find the file.
+"""
 
 resource_dir = Path(__file__).resolve().parent.parent #testing male-seed-beetle returned with print(f'{script_dir}')
 
 folder_path = resource_dir / 'data-raw' #testing male-seed-beetle/data-raw returned with print(f'{folder_path}')
+
+url = 'https://datadryad.org/stash/downloads/file_stream/30903'
 
 raw_data = requests.get(url, allow_redirects=True)
 
