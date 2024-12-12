7a8ebc696a53aa372205f0f750741bb9584f84c9
Luke W. Johnston <lwjohnst@gmail.com>
2024-12-12 13:29:32 +0100

    style: :art: reformat Python script

--- male-seed-beetle/scripts/download-data.py
+++ male-seed-beetle/scripts/download-data.py
@@ -1,23 +1,25 @@
+"""Downloads a data file from a given URL.
+
+This is the most basic of the download data scripts. It requires that the url for
+the data is provided as an argument, and it assumes that a data-raw folder
+has been created containing a .gitignore file.
+"""
+
 from pathlib import Path
 
 import requests
 
-"""Downloads a data file from a given URL.
+# testing male-seed-beetle returned with print(f'{script_dir}')
+resource_dir = Path(__file__).resolve().parent.parent
 
-This is the most basic of the download data scripts. It requires that the url for 
-the data is provided as an argument, and it assumes that a data-raw folder 
-has been created containing a .gitignore file.
-"""
+# testing male-seed-beetle/data-raw returned with print(f'{folder_path}')
+folder_path = resource_dir / "data-raw"
 
-resource_dir = Path(__file__).resolve().parent.parent #testing male-seed-beetle returned with print(f'{script_dir}')
-
-folder_path = resource_dir / 'data-raw' #testing male-seed-beetle/data-raw returned with print(f'{folder_path}')
-
-url = 'https://datadryad.org/stash/downloads/file_stream/30903'
+url = "https://datadryad.org/stash/downloads/file_stream/30903"
 
 raw_data = requests.get(url, allow_redirects=True)
 
-file_path = folder_path / 'data.csv'
+file_path = folder_path / "data.csv"
 
-with open(file_path, 'wb') as file:
-    file.write(raw_data.content) 
+with open(file_path, "wb") as file:
+    file.write(raw_data.content)
