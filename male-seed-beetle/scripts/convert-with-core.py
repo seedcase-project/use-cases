78126f40662b2397fb68e9c1dd6226ec3a8b4a80
Kris Beicher <112945740+K-Beicher@users.noreply.github.com>
2024-12-20 09:47:20 +0100

    feat: write code to convert from categorical int to character

--- male-seed-beetle/scripts/convert-with-core.py
+++ male-seed-beetle/scripts/convert-with-core.py
@@ -17,11 +17,28 @@ Path('../data-raw/data.csv').unlink()
 
 Path('../data-raw/data1.csv').rename('../data-raw/data.csv')
 
-#Find the columns with type string, then check for pure numerical 
-# values as that may indicate that there is a conversion error
-
 df = pl.read_csv('../data-raw/data.csv')
 
+#Convert categorical values to characters
+
+block_mapping = {1:"Block 1", 2:"Block 2", 3:"Block 3"}
+
+df = df.with_columns(
+    BLOCK = pl.col("BLOCK").replace_strict(block_mapping, return_dtype=pl.Utf8)
+)
+df.write_csv('../data-raw/data.csv')
+
+#Update from scream to lowercase, as that will create snake_case in this file
+#df = pl.read_csv('../data-raw/data.csv')
+
+column_names = {col: col.lower() for col in df.columns}
+
+df = df.rename(column_names)
+
+df.write_csv('../data-raw/data.csv')
+
+#Find the columns with type string, then check for pure numerical 
+# values as that may indicate that there is a conversion error
 for col, dtype in zip(df.columns, df.dtypes):
     if dtype == pl.Utf8:
         print(f"{col}: {dtype}")
@@ -29,16 +46,7 @@ for col, dtype in zip(df.columns, df.dtypes):
         for value in df[col]:
             if value.isdigit():
                 print(f"Numerical value found in column {col}: {value}")
-
-#Update from scream to lowercase, as that will create snake_case in this file
-
-df = pl.read_csv('../data-raw/data.csv')
-
-column_names = {col: col.lower() for col in df.columns}
-
-df = df.rename(column_names)
-
-df.write_csv('../data-raw/data.csv')
+                #TODO Need to rework this bit
 
 # Update abbreviated values to full text
 treatment_map = {"V":"Virgin", "M":"Mating"}
@@ -47,26 +55,3 @@ df = df.with_columns(
     treatment=pl.col("treatment").replace_strict(treatment_map)
 )
 df.write_csv('../data-raw/data.csv')
-
-'''
-"CYCLE":"FOCAL_CYCLE", 
-"O2":"02_Consumed", 
-"CO2":"C02_Produced", 
-"RQ":"Respiratory_quotient", 
-"BLOCK":"Experimental_Block", 
-"ACTIVITY":"ACTIVITY", 
-"STRAIN":"STRAIN", 
-"MT_DNA_HAPLOTYPE":"Mitochodrial_DNA_Haplotype", 
-"NC_GENOTYPE":"Nuclear_Lineage", 
-"TREATMENT":"TREATMENT", 
-"COEVOL":"Coadaptation", 
-"W_V":"Virgin_Body_Weight", 
-"W_M1":"Weight_after_Mating1", 
-"W_M1R":"WaM1_after_Respirometry", 
-"EJAC1":"Ejaculate_Weight_First_Mating", 
-"EJAC2":"Ejaculate_Weight_Second_Mating", 
-"COPULDUR_1":"Copulation_Duration_First_Mating", 
-"COPULDUR_2":"Copulation_Duration_Second_Mating", 
-"ID":"ID"
-'''
-
